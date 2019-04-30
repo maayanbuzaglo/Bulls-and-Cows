7e6d24b780f7475f4d0c6c1311027a3d484b239b
maayanbuzaglo <maayan13500@gmail.com>
2019-04-30 15:27:33 +0300

    add tests

--- Test.cpp
+++ Test.cpp
@@ -50,37 +50,51 @@ int main() {
 		}
 
 
-		// Our test
-		testcase.setname("Calculate bull and pgia")
-		.CHECK_OUTPUT(calculateBullAndPgia("1111","1212"), "2,0")      // 2 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("1212","2121"), "0,4")      // 0 bull, 4 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("1111","1111"), "4,0")      // 4 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("1111","3456"), "0,0")      // 0 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("1111","1212"), "2,0")      // 2 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3674","8462"), "0,2")      // 0 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3674","4773"), "1,1")      // 1 bull, 1 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("732846","123456"), "1,3")      // 1 bull, 3 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("23476","26437"), "2,3")      // 2 bull, 3 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("4424","2344"), "1,2")      // 1 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3452","3456"), "3,0")      // 3 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3568","5381"), "0,3")      // 0 bull, 3 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("0000","0000"), "4,0")      // 4 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("2386","1212"), "2,0")      // 2 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("872993","629743"), "1,3")      // 1 bull, 3 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("81364","18436"), "0,5")      // 0 bull, 5 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("81364","81364"), "5,0")      // 5 bull, 0 pgia
+		// Our tests:
+		testcase.setname("Our tests - Calculate bull and pgia")
+		.CHECK_OUTPUT(calculateBullAndPgia("1111","1212"), "2,0") // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1212","2121"), "0,4") // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1111","1111"), "4,0") // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1111","3456"), "0,0") // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1111","1212"), "2,0") // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3674","8462"), "0,2") // 0 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3674","4773"), "1,1") // 1 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("732846","123456"), "1,3") // 1 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("23476","26437"), "2,3") // 2 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("4424","2344"), "1,2") // 1 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3452","3456"), "3,0") // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3568","5381"), "0,3") // 0 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("0000","0000"), "4,0") // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("2386","1212"), "2,0") // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("872993","629743"), "1,3") // 1 bull, 3 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("81364","18436"), "0,5") // 0 bull, 5 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("81364","81364"), "5,0") // 5 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("64321010","61910297"), "1,4")      // 1 bull, 4 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("21","11"), "1,0")      //1 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("123","321"), "1,2")      // 1 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("6374","6336"), "2,0")      // 2 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("1475","8932"), "0,0")      // 0 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("1475","1457"), "2,2")      // 2 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("5686","2436"), "1,0")      // 1 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("4655","6888"), "0,1")      // 0 bull, 1 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("6665","6661"), "3,0")      // 3 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("6892","0862"), "2,1")      // 2 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("21","11"), "1,0") //1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("123","321"), "1,2") // 1 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("6374","6336"), "2,0") // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1475","8932"), "0,0") // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1475","1457"), "2,2") // 2 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("5686","2436"), "1,0") // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("4655","6888"), "0,1") // 0 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("6665","6661"), "3,0") // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("6892","0862"), "2,1") // 2 bull, 1 pgia
 		;
 
+		ConstantChooser c0000{"0000"}, c9876{"9876"}, c123{"123"};
+		ConstantGuesser g1010{"1010"}, g6789{"6789"}, g123{"123"};
+
+		testcase.setname("Play with dummy choosers and guessers")
+		.CHECK_EQUAL(play(c0000, g1010, 4, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c9876, g9999, 4, 1), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c123, g12345, 3, 100), 101)   // guesser loses technically by making an illegal guess (too long).
+		.CHECK_EQUAL(play(c123, g12345, 5, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+
+		.CHECK_EQUAL(play(c123, g1010, 4, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c9876, g9999, 4, 100), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c123, g123, 3, 100), 1)      // guesser wins in one turn..
+		.CHECK_EQUAL(play(c123, g12345, 5, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		;
 
 
     grade = testcase.grade();
